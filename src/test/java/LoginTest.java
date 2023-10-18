4c0f407f318ae6978c425cb755dc15fb3ad04049
Cristian Rodríguez <cr113022@gmail.com>
2023-10-18 13:39:05 +0200

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,42 @@
+import org.example.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        driver = getDriver();
+    }
+
+
+    @Test//T1
+    public void canNotLoginWithInvalidPassword() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        assertTrue(loginPage.isLoaded());
+        loginPage.login("gabdimitrievski111@gmail.com", "wrong_password_test");
+        String errorMessage = loginPage.getErrorMessage();
+        assertEquals(errorMessage, "The password you’ve entered is incorrect. Forgot Password?");
+
+    }
+
+
+
+    private WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        return new ChromeDriver();
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
+}
